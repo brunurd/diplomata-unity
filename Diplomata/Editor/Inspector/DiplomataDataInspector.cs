bcd8f862a0bc6061f1fde04037df75847150ae5e
Bruno Araujo <araujo.contato@gmail.com>
2018-08-15 00:30:04 -0300

    DiplomataDataInspector fix

--- Diplomata/Editor/Inspector/DiplomataDataInspector.cs
+++ Diplomata/Editor/Inspector/DiplomataDataInspector.cs
@@ -5,14 +5,14 @@ using UnityEngine;
 
 namespace DiplomataEditor.Inspector
 {
-  [UnityEditor.CustomEditor(typeof(DiplomataData))]
+  [CustomEditor(typeof(DiplomataData))]
   public class DiplomataManagerInspector : Editor
   {
     public override void OnInspectorGUI()
     {
-      UnityEditor.EditorGUILayout.HelpBox("\nthis auto-generated file is a object to store all Diplomata data.\n\n" +
+      EditorGUILayout.HelpBox("\nthis auto-generated file is a object to store all Diplomata data.\n\n" +
         "The object instantiate just one time in the game build in the first scene it's appear. (It's a Singleton)\n\n" +
-        "The real data are stored in the resources folder, so don't worry if you need to delete this object during development.\n", UnityEditor.MessageType.Info);
+        "The real data are stored in the resources folder, so don't worry if you need to delete this object during development.\n", MessageType.Info);
     }
   }
 }
