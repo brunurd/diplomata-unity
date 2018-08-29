433bd0a2fb4d76a6b8df3ac3f5c36b78bdcda37e
Bruno Araujo <araujo.contato@gmail.com>
2018-08-29 11:24:05 -0300

    DiplomataData update

--- Diplomata/DiplomataData.cs
+++ Diplomata/DiplomataData.cs
@@ -3,6 +3,7 @@ using System.Linq;
 using Diplomata.Helpers;
 using Diplomata.Models;
 using Diplomata.Persistence;
+using Diplomata.Persistence.Models;
 using UnityEngine;
 
 namespace Diplomata
@@ -94,7 +95,67 @@ namespace Diplomata
       }
     }
 
-    public static Message GetMessageById(string uniqueId)
+    /// <summary>
+    /// Method to dispose the game data for new game.
+    /// </summary>
+    public static void DisposeData()
+    {
+      DestroyImmediate(instance.gameObject);
+    }
+
+    /// <summary>
+    /// Get a global flag by name.
+    /// </summary>
+    /// <param name="name">The name of the flag.</param>
+    /// <returns>The <seealso cref="Diplomata.Models.Flag"> object or null.</returns>
+    public static Flag GetFlag(string name)
+    {
+      return globalFlags.Find(globalFlags.flags, name);
+    }
+
+    /// <summary>
+    /// Get a character by his name.
+    /// </summary>
+    /// <param name="name">The character name.</param>
+    /// <returns>The <seealso cref="Diplomata.Models.Character"> object or null.</returns>
+    public static Character GetCharacter(string name)
+    {
+      return Character.Find(characters, name);
+    }
+
+    /// <summary>
+    /// Get a context by it's index.
+    /// </summary>
+    /// <param name="character">The character parent of the context.</param>
+    /// <param name="contextIndex">The index of the context.</param>
+    /// <returns>The <seealso cref="Diplomata.Models.Context"> object or null.</returns>
+    public static Context GetContext(Character character, int contextIndex)
+    {
+      return Context.Find(character, contextIndex);
+    }
+
+    /// <summary>
+    /// Get a message by it's context and row index.
+    /// </summary>
+    /// <param name="context">The context parent of the message.</param>
+    /// <param name="columnIndex">The index of the column.</param>
+    /// <param name="rowIndex">The index of the row in the column.</param>
+    /// <returns></returns>
+    public static Message GetMessage(Context context, int columnIndex, int rowIndex)
+    {
+      var column = Column.Find(context, columnIndex);
+      if (column == null) return null;
+      var message = Message.Find(column.messages, rowIndex);
+      if (message != null) return message;
+      return null;
+    }
+
+    /// <summary>
+    /// Get a context by it's unique id.
+    /// </summary>
+    /// <param name="uniqueId">The unique id (a guid string).</param>
+    /// <returns>The <seealso cref="Diplomata.Models.Message"> object or null.</returns>
+    public static Message GetMessage(string uniqueId)
     {
       foreach (Character character in characters)
       {
@@ -102,51 +163,70 @@ namespace Diplomata
         {
           foreach (Column column in context.columns)
           {
-            foreach (Message message in column.messages)
-            {
-              if (message.GetUniqueId() == uniqueId)
-              {
-                return message;
-              }
-            }
+            var message = Message.Find(column.messages, uniqueId);
+            if (message != null) return message;
           }
         }
       }
       return null;
     }
 
-    public static List<Message> GetMessagesByLabel(Context context, string labelName)
+    /// <summary>
+    /// Get a quest by it's name.
+    /// </summary>
+    /// <param name="name">The quest name.</param>
+    /// <returns>The <seealso cref="Diplomata.Models.Quest"> object or null.</returns>
+    public static Quest GetQuest(string name)
     {
-      string labelId = null;
-      List<Message> messages = new List<Message>();
+      return Quest.Find(quests, name);
+    }
 
-      foreach (Label label in context.labels)
-      {
-        if (label.name == labelName)
-        {
-          labelId = label.uniqueId;
-        }
-      }
+    /// <summary>
+    /// Get a item by it's name.
+    /// </summary>
+    /// <param name="name">The item name.</param>
+    /// <param name="language">The language of this name, if empty uses the options first language.</param>
+    /// <returns>The <seealso cref="Diplomata.Models.Item"> object or null.</returns>
+    public static Item GetItem(string name, string language = "")
+    {
+      if (language == "") language = options.languages[0].name;
+      return Item.Find(inventory.items, name, language);
+    }
 
-      if (labelId != null)
-      {
-        foreach (Column column in context.columns)
-        {
-          foreach (Message message in column.messages)
-          {
-            if (message.labelId == labelId)
-            {
-              messages.Add(message);
-            }
-          }
-        }
-      }
-      else
-      {
-        Debug.LogWarning("No label with the name \"" + labelName + "\"");
-      }
+    /// <summary>
+    /// Return all characters persistent 
+    /// </summary>
+    /// <returns>A array of characters persistent </returns>
+    public static CharacterPersistentContainer GetPersistentCharacters()
+    {
+      return new CharacterPersistentContainer(Persistence.Data.GetArrayData<CharacterPersistent>(characters.ToArray()));
+    }
 
-      return messages;
+    /// <summary>
+    /// Return all interactables persistent 
+    /// </summary>
+    /// <returns>A array of interactables persistent </returns>
+    public static InteractablePersistentContainer GetPersistentInteractables()
+    {
+      return new InteractablePersistentContainer(Persistence.Data.GetArrayData<InteractablePersistent>(interactables.ToArray()));
+    }
+
+    /// <summary>
+    /// Return all quests persistent 
+    /// </summary>
+    /// <returns>A array of quests persistent </returns>
+    public static QuestPersistentContainer GetPersistentQuests()
+    {
+      return new QuestPersistentContainer(Persistence.Data.GetArrayData<QuestPersistent>(quests));
+    }
+
+    /// <summary>
+    /// Return all talk logs persistent 
+    /// </summary>
+    /// <returns>A array of talk logs persistent </returns>
+    public static TalkLogPersistentContainer GetPersistentTalkLogs()
+    {
+      return new TalkLogPersistentContainer(Persistence.Data.GetArrayData<TalkLogPersistent>(talkLogs));
     }
   }
 }
