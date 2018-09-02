1a10e25f03a365df1bafb1c36ebb8089b75a530b
Bruno Araujo <araujo.contato@gmail.com>
2018-09-02 04:55:43 -0300

    Undo and Watcher helpers added

---
+++ Diplomata/Editor/Helpers/UndoHelper.cs
@@ -0,0 +1,38 @@
+using UnityEditor;
+using UnityEngine;
+
+namespace Diplomata.Editor.Helpers
+{
+  public static class UndoHelper
+  {
+    private static object obj;
+    private static object cachedValue;
+
+    public static void EventListener<T>(object obj, ref T value)
+    {
+      Event e = Event.current;
+
+      if (e != null)
+      {
+        if (e.control)
+        {
+          Undo.ClearAll();
+        }
+
+        if (e.control && e.keyCode == KeyCode.Z && obj.Equals(UndoHelper.obj))
+        {
+          value = (T) cachedValue;
+          GUI.SetNextControlName("noFocus");
+          GUI.Label(new Rect(-100, -100, 1, 1), "");
+          GUI.FocusControl("noFocus");
+        }
+      }
+    }
+
+    public static void AddObject(object obj, object value)
+    {
+      UndoHelper.obj = obj;
+      cachedValue = value;
+    }
+  }
+}
---
+++ Diplomata/Editor/Helpers/Watcher.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+namespace Diplomata.Editor.Helpers
+{
+  [ExecuteInEditMode]
+  public class Watcher : ScriptableObject
+  {
+    private List<bool> flags;
+    private Action action;
+
+    public void Set(Action action, params bool[] flags)
+    {
+      this.action = action;
+      foreach (var flag in flags)
+      {
+        this.flags.Add(flag);
+      }
+    }
+
+    private bool GetFlags()
+    {
+      foreach (var flag in flags)
+      {
+        if (!flag) return false;
+      }
+      return true;
+    }
+
+    private void Update()
+    {
+      if (GetFlags())
+      {
+        action();
+      }
+    }
+  }
+}
